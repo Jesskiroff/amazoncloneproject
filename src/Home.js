6a4332e0385586d87689d5d1bde045b99e7cedf7
Jessica Kiroff <jesskiroff@gmail.com>
2024-05-13 12:32:20 -0400

    fix product css

--- src/Home.js
+++ src/Home.js
@@ -6,14 +6,15 @@ function Home() {
   return (
     <div className='home'>
       <div className='home__container'>
-      <img
-        className='home__image'
-        src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
-        alt=""
-      />
-      </div>
+        <img
+            className='home__image'
+            src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
+            alt=""
+        />
+        
 
       <div classname="home__row">
+        <Product/>
         <Product/>
         
       </div>
@@ -29,6 +30,8 @@ function Home() {
       </div>
 
     </div>
+    </div>
+
   )
 }
 
