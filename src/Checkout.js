4e578e6f2c9b9fc4c162e85949b1e1847b72551a
Jessica Kiroff <jesskiroff@gmail.com>
2024-05-21 16:12:33 -0400

    add checkout ad image

--- src/Checkout.js
+++ src/Checkout.js
@@ -2,8 +2,10 @@ import React from 'react';
 import './Checkout.css';
 function Checkout() {
   return (
-    <div>
-      <h1>Like This!</h1>
+    <div className="checkout">
+      <div className="checkout__left">
+        <img className="checkout__ad" src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg" alt="" />
+      </div>
     </div>
   );
 }
