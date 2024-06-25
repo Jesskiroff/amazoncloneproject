6c02e5b0cd6d6d698325a75510875583ef85aaf0
Jessica Kiroff <jesskiroff@gmail.com>
2024-06-25 16:05:39 -0400

    create reducer file

---
+++ src/reducer.js
@@ -0,0 +1,19 @@
+// just think about this a s the data layer
+
+export const initialState= {
+    basket: [],
+};
+
+const reducer =(state, action) => {
+    switch(action.type){
+        case 'ADD_TO_BASKET':
+            return {
+                ...state,
+                basket:[...state.basket, action.item]
+            };
+        default:
+            return state;
+    }
+};
+
+export default reducer;
